f6ec980a02a553413490561f02547d88b25ebbfa
ziyu <4a7g0119@stust.edu.tw>
2022-04-01 16:02:41 +0800

    Initial Commit

---
+++ money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  money
+//
+//  Created by 黃子瑜 on 2022/4/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
